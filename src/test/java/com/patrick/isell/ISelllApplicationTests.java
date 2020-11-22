b287cb2fc7e111be3f497f49f31a2843cb369345
PatrickMutugi <patrickmutugi5@gmail.com>
2020-11-22 09:28:27 +0300

    up

---
+++ src/test/java/com/patrick/isell/ISelllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrick.isell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ISelllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
